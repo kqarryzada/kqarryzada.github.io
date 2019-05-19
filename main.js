f331ee4189ec482a64df11b414083e18ab3bc99b
kqarryzada <khalid.qarryzada@gmail.com>
2019-05-18 20:11:47 -0500

    Implemented blinking cursor in 'About me', changed color to reflect a terminal

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var cursorOn = "$ _";
 var cursorOff= "$";
 var toggle = false;
+
 function blinkCursor() {
     if(toggle)
         document.getElementById("cursor").innerHTML = cursorOn;
@@ -8,3 +9,8 @@ function blinkCursor() {
         document.getElementById("cursor").innerHTML = cursorOff;
     toggle = !toggle  
 }
+
+// Every 0.5s, make the cursor "blink"
+setInterval(function() {
+    blinkCursor()
+}, 500);
