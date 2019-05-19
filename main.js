1530ab7dba066d8c7d7b095e13b92f21996231d4
kqarryzada <khalid.qarryzada@gmail.com>
2019-05-19 13:05:45 -0500

    Social media now links to other pages

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-var cursorOn = "$ _";
-var cursorOff= "$";
-var toggle = false;
+// Web links
+const email = "mailto:khalid.qarryzada@gmail.com";
+const linkedInURL = "https://www.linkedin.com/in/khalid-qarryzada/";
+const githubURL = "https://github.com/kqarryzada";
+const repoURL ="https://github.com/kqarryzada/kqarryzada.github.io";
+const stkOverflowURL ="https://stackoverflow.com/users/11386070/kqarryzada?tab=profile";
+
+// Cursor logic
+const cursorOn = "$ _";
+const cursorOff = "$";
+let toggle = false;
 
 function blinkCursor() {
     if(toggle)
@@ -10,7 +18,32 @@ function blinkCursor() {
     toggle = !toggle  
 }
 
-// Every 0.5s, make the cursor "blink"
-setInterval(function() {
-    blinkCursor()
-}, 500);
+function openEmail() {
+    window.open(email);
+}
+
+function openLinkedIn() {
+    window.open(linkedInURL);
+}
+
+function openGithub() {
+    window.open(githubURL);
+}
+
+function openWebSourceCode() {
+    window.open(repoURL);
+}
+
+function openStackOverflow() {
+    window.open(stkOverflowURL);
+}
+
+function main() {
+    // Every 0.5s, make the cursor "blink"
+    setInterval(function() {
+        blinkCursor()
+    }, 500);
+}
+
+
+main();
